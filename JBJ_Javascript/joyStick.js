5950c339eeb96230ebfc38bb7f8088b665285ca9
LAPTOP-EJ3Q9SO5\ByeongjaeJeon <jbjeon0113@gmail.com>
2021-01-13 22:56:17 +0900

    solve: 조이스틱
    비슷한 걸 네이버 부캠 들어갈 때 풀었던 거 같은데 여튼 탐욕법

---
+++ JBJ_Javascript/joyStick.js
@@ -0,0 +1,42 @@
+function solution(name) {
+  let answer = 0;
+  let upDownCount = [];
+  const nameArr = name.split("");
+
+  nameArr.forEach((elem) => {
+    upDownCount.push(
+      Math.min(
+        elem.charCodeAt(0) - "A".charCodeAt(0),
+        "Z".charCodeAt(0) - elem.charCodeAt(0) + 1
+      )
+    );
+  });
+
+  let index = 0;
+  while (true) {
+    answer += upDownCount[index];
+    upDownCount[index] = 0;
+    let sumOfArr = upDownCount.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    if (sumOfArr === 0) break;
+    let left = 0,
+      right = 0;
+    (leftIndex = index), (rightIndex = index);
+    while (upDownCount[leftIndex] <= 0) {
+      left++;
+      if (leftIndex <= 0) leftIndex = upDownCount.length - 1;
+      else leftIndex--;
+    }
+    while (upDownCount[rightIndex] <= 0) {
+      right++;
+      rightIndex = (rightIndex + 1) % upDownCount.length;
+    }
+    answer += left < right ? left : right;
+    index = left < right ? leftIndex : rightIndex;
+  }
+
+  return answer;
+}
+
+console.log(solution("JEROEN"));
